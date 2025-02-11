fbbb49aaee500f25a1e3217805689d33e02a91bc
RohanKumarJha <rohan.jha0107@gmail.com>
2025-02-11 11:53:01 +0530

    arrayList

--- ArrayList/Question.java
+++ ArrayList/Question.java
@@ -1,6 +1,6 @@
 
 public class Question {
-    public static void main(String[] args) {
+    public static void main(String[] args) {   
 
         // Operation 1
         // System.out.println("Enter the elements of an arrayList");
